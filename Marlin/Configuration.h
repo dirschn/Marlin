2f726ac2a7f3f812991ef7bfa1df35ea07455aef
Nick Dirschel <dirschn@gmail.com>
2022-10-10 19:06:32 -0400

    hot end autopid tune

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -652,9 +652,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp 32.83
-    #define DEFAULT_Ki 3.39
-    #define DEFAULT_Kd 79.43
+    #define DEFAULT_Kp 26.36
+    #define DEFAULT_Ki 2.52
+    #define DEFAULT_Kd 69.07
     #define DEFAULT_bedKp 126.73
     #define DEFAULT_bedKi 24.75
     #define DEFAULT_bedKd 432.57
